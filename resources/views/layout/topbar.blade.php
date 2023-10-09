4d6f42e452a42c98d7c14adc075b993ec3f594ee
HakamFebta <hakamputro@gmail.com>
2023-10-09 23:26:59 +0700

    Js void(0) Profil topbar

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -388,7 +388,8 @@
             </button>
             <div class="dropdown-menu dropdown-menu-end">
                 <!-- item-->
-                <a class="dropdown-item" href="#"><i class="bx bx-user font-size-16 align-middle me-1"></i>
+                <a class="dropdown-item" href="javascript:void(0);"><i
+                        class="bx bx-user font-size-16 align-middle me-1"></i>
                     <span key="t-profile">Profile</span></a>
                 {{-- <a class="dropdown-item" href="#"><i class="bx bx-wallet font-size-16 align-middle me-1"></i>
                     <span key="t-my-wallet">My Wallet</span></a> --}}
