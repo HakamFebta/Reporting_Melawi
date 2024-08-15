3b9aabc63e99b66909b4e0f7751f055b3d4f2bf3
HakamFebta <hakamputro@gmail.com>
2024-08-15 20:43:33 +0700

    Perbaikan template

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8" />
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
